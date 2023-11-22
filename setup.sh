778293022cfb4db442719ad45d999acdc6704359
Animesh Sharma <ash022@login-1.saga>
2023-11-22 16:01:56 +0100

    run over saga cluster

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #wget "https://figshare.com/ndownloader/files/36246219" -O alphaPepDL.zip 
 git clone https://github.com/animesh/alphapeptdeep
 cd alphapeptdeep/
-pip install peptdeep --user
+conda create --name peptdeep python=3.9 -y
+conda activate peptdeep
+pip install -e ".[development]"
 $HOME/.local/bin/peptdeep -h
 $HOME/.local/bin/peptdeep  export-settings settings.yaml
-ln -s /home/ash022/data/NORSTORE_OSL_DISK/NS9036K/peptdeep $HOME/.
-/home/ash022/.local/bin/peptdeep library settings.yaml 
+mkdir /cluster/projects/nn9036k/peptdeep
+$HOME/.conda/envs/peptdeep/bin/peptdeep library settings.yaml
