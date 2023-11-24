1fe52af2a63c080e0a594f06de67d047c4b2eb03
ani <sharma.animesh@gmail.com>
2023-11-24 16:06:20 +0100

    fix gpu comp at nird

--- setup.sh
+++ setup.sh
@@ -6,7 +6,9 @@ conda activate peptdeep
 pip install -e ".[development]"
 $HOME/.local/bin/peptdeep -h
 $HOME/.local/bin/peptdeep  export-settings settings.yaml
+#nird
 mkdir /cluster/projects/nn9036k/peptdeep
-$HOME/.conda/envs/peptdeep/bin/peptdeep library settings.yaml
-
-how 
+rm -rf $HOME/.local/lib/python3.* $HOME/.local/include/python3.* $HOME/.local/bin/peptdeep $HOME/.conda/envs/peptdeep/
+pip install --user peptdeep
+peptdeep library settings.nird.yaml 
+$HOME/.local/bin/peptdeep library settings.nird.yaml 
